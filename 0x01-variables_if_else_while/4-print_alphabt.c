6376420c2cd1be0031336d9ebc4063a60eeb004a
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-06-18 16:53:08 -0700

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - main program Gets a random number and  compare it with 5
+* Return: 0
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
