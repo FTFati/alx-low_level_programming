fb70d27e5fd66101ab684dcc9c94fa6d61c18762
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-06-20 16:02:47 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+* Retuen: void
 */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
+	{
+	_putchar(letter);
+	}
 	_putchar('\n');
 }
