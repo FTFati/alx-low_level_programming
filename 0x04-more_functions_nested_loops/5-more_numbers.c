135f73e8a6c453e4c578b80e0f67e40af0ee72b4
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-06-24 08:51:54 -0700

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
 * more_numbers -  prints 10 times the numbers, from 0 to 14
 *
-* Return: Always 0.
+* Return: void
 */
 
 void more_numbers(void)
@@ -16,7 +16,9 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
+			{
 				_putchar((j / 10) + '0');
+			}
 			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
