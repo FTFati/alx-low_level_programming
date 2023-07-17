2eea50529314fab49a9bebf77f2c2c10e1edc78b
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-07-17 08:38:34 -0700

    4 error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,34 +1,49 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * *new_dog - function that creates a new dog
+ * new_dog - function that creates a new dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  * Return: pointer
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j, k;
 	dog_t *p;
-	/* reserving memory to struct*/
+
 	p = malloc(sizeof(dog_t));
+
 	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
 	{
 		free(p);
-		free(owner);
 		return (NULL);
 	}
-	if (owner == NULL)
+	for (i = 0; name[i]; i++)
+		;
+	for (j = 0; owner[j]; j++)
+		;
+	p->name = malloc(i + 1);
+	p->owner = malloc(j + 1);
+
+	if (p->name == NULL || p->owner == NULL)
 	{
-		free(p);
-		free(name);
+		free(p->name), free(p->owner), free(p);
 		return (NULL);
 	}
-	p->name = name;
+	for (k = 0; k < i; k++)
+	{
+		p->name[k] = name[k];
+	}
+	p->name[k] = '\0';
+	for (k = 0; k < j; k++)
+	{
+		p->owner[k] = owner[k];
+	}
+	p->owner[k] = '\0';
 	p->age = age;
-	p->owner = owner;
 	return (p);
 }
