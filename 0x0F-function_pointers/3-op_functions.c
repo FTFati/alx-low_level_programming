3ade1ef0e39d113252ca9958a76e21e4b4a24874
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-07-19 10:26:26 -0700

    erroooor

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,43 +1,65 @@
-#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
- * op_add - addition operator
- * @a: first int
- * @b: second int
- * Return: result of 2 numbers
+ * op_add - add
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtract
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiply
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divide
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
+	if (!b)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(100);
 	}
 	return (a / b);
 }
 
+/**
+ * op_mod - modulo
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
+	if (!b)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(100);
 	}
 	return (a % b);
