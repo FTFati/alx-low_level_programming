3c91a33d0fa291c2a08dd7c2ceca16b33eff3b24
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-07-19 10:16:33 -0700

    error2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,12 +5,14 @@
 * @array: array of integers
 * @size: size of array
 * @action: function pointer
+* Return: Void
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+	{
+		while (size--)
+			action(*array++);
+	}
 }
