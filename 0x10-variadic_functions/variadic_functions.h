ad845606e0054d0bf9aedab4d1eee31c4c69e41f
Fatima Amkachou <fatima.amkachou@gmail.com>
2023-07-20 09:27:59 -0700

    error

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,15 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+/**
+ * struct print - print all types
+ * @x: char Type of print
+ * @T_func: funcion to use
+ */
+typedef struct print
+{
+	char *x;
+	void (*T_func)(va_list);
+} t_print;
+
 #endif
