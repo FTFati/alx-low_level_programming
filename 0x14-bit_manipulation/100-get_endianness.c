12f87e53e332685c9dc85a2f281d9fc0a93b7975
FT/Fati <fatima.amkachou@gmail.com>
2023-08-31 17:19:27 -0700

    advanced100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * get_endianness - function that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+	return ((int) (((char *)&i)[0]));
+}
